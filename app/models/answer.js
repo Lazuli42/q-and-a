d6a58eb3e163a7c54f9e92626f23246b1e6d1b44
Zachary Baum <zacharybaum42@gmail.com>
2016-10-28 10:24:41 -0400

    display questions with answers

--- app/models/answer.js
+++ app/models/answer.js
@@ -3,4 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   author: DS.attr(),
   text: DS.attr(),
+  question: DS.belongsTo('rental', { async: true })
 });
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
+var questions = [{
+  title: "how do i ember?",
+  author: "jsNoob",
+  notes: "idk how to google",
+  answers: [{
+    author: "jsGuru",
+    text: "have you tried turning it off and on again?"
+    }, {
+    author: "veteranCoder",
+    text: "i'd link you to the documentation, but it doesn't seem to exist."
+  }]
+}]
+
 export default Ember.Route.extend({
+  model() {
+    return questions;
+  }
 });
